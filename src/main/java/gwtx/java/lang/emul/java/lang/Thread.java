1052bcb63ecdc9c141949883c081163d56724493
Wilson <ttt43ttt@126.com>
2016-01-10 23:20:48 +0800

    implement few methods of java.lang.Thread.

--- src/main/java/gwtx/java/lang/emul/java/lang/Thread.java
+++ src/main/java/gwtx/java/lang/emul/java/lang/Thread.java
@@ -2,10 +2,53 @@ package java.lang;
 
 public class Thread implements Runnable {
 
+    /* For autonumbering anonymous threads. */
+    private static int threadInitNumber;
+
+    private static synchronized int nextThreadNum() {
+        return threadInitNumber++;
+    }
+
+    private String name;
+
+    private Runnable target;
+
+    public Thread() {
+        init(null, "Thread-" + nextThreadNum(), 0);
+    }
+
+    public Thread(Runnable target) {
+        init(target, "Thread-" + nextThreadNum(), 0);
+    }
+
+    private void init(Runnable target, String name, long stackSize) {
+        this.target = target;
+        this.name = name;
+    }
+
     @Override
     public void run() {
-        // TODO Auto-generated method stub
+        if (target != null) {
+            target.run();
+        }
+    }
 
+    public synchronized void start() {
+        this.run();
+    }
+
+    /**
+     * This will block JS.
+     * 
+     * @param millis
+     * @throws InterruptedException
+     */
+    @Deprecated
+    public static void sleep(long millis) throws InterruptedException {
+        long target = System.currentTimeMillis() + millis;
+        while (System.currentTimeMillis() < target) {
+            // block JS
+        }
     }
 
 }
